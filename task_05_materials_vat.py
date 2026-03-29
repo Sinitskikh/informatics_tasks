27046fcb9b1baaccd23e29f519f357fca8ef6ed7
Sinitskikh <sinitskih@bk.ru>
2026-03-29 19:01:28 +0300

    added basic logic

--- task_05_materials_vat.py
+++ task_05_materials_vat.py
@@ -4,3 +4,18 @@
 Задание: Рассчитайте стоимость покупки материалов с прогрессивной системой скидок
 Цель: Изучение арифметических операций и работы переменных
 """
+
+price = 2000.0
+quantity = 2
+print("Стоимость: " + str(price) + " Количество: " + str(quantity))
+total_price = price * quantity
+print("Итого: " + str(total_price))
+
+if total_price >= 1000 and total_price <= 5000:
+    final_price = total_price - (total_price*0.05)
+elif total_price >= 5000:
+    final_price = total_price - (total_price*0.1)
+else:
+    final_price = total_price
+
+print(final_price)
