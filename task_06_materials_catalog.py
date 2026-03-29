d59a9cb57bb96092b49bb03d750b52d3ebd6d066
Sinitskikh <sinitskih@bk.ru>
2026-03-29 19:23:37 +0300

    added comments

--- task_06_materials_catalog.py
+++ task_06_materials_catalog.py
@@ -5,19 +5,27 @@
 Цель: Индексация, срезы, добавления, удаление элементов
 """
 
+# Создаем список материалов
 materials = ["Дерево", "Мрамор", "Кварц", "Пластик", "Бетон"]
 
+# Выводим первый и последний элементы
 print(materials[0])
 print(materials[len(materials) - 1])
 
+# Выводим элементы в середине списка
 for i in range(1, len(materials) - 1):
     print(materials[i])
 
+# Разделитель
 print("")
+
+# Добавляем два новых материала
 materials.append("Камень")
 materials.append("Стекло")
 
+# Удаляем второй элемент
 del materials[1]
 
+# Выводим все элементы нового списка
 for i in range(0, len(materials)):
     print(materials[i])
