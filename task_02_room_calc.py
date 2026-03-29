91c4fe346e974c85473977907d33d21ff49dda88
Sinitskikh <sinitskih@bk.ru>
2026-03-29 17:42:20 +0300

    added logic to second file

--- task_02_room_calc.py
+++ task_02_room_calc.py
@@ -5,5 +5,18 @@
 Цель: Демонстрация использования алгебраических функций.
 """
 
+length = 10.5
+width = 12.4
+height = 3
 
+walls = length*height + width*height
+floor = width*length
+volume = length*width*height
 
+print("Площадь стен: " + str(round(walls, 2)))
+print("Площадь пола: " + str(round(floor, 2 )))
+print("Объем: " + str(round(volume, 2)))
+
+price = round(walls*125, 2)
+
+print("Стоимость покраски стен: " + str(price))
