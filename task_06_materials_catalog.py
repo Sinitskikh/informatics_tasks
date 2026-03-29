6d48a598c4ed2f8a09078e8868cf7010236fdec2
Sinitskikh <sinitskih@bk.ru>
2026-03-29 19:21:27 +0300

    added logic

--- task_06_materials_catalog.py
+++ task_06_materials_catalog.py
@@ -5,3 +5,19 @@
 Цель: Индексация, срезы, добавления, удаление элементов
 """
 
+materials = ["Дерево", "Мрамор", "Кварц", "Пластик", "Бетон"]
+
+print(materials[0])
+print(materials[len(materials) - 1])
+
+for i in range(1, len(materials) - 1):
+    print(materials[i])
+
+print("")
+materials.append("Камень")
+materials.append("Стекло")
+
+del materials[1]
+
+for i in range(0, len(materials)):
+    print(materials[i])
