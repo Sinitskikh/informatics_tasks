bd62e96125d34693b4e085740b320dc75aa0e1f9
Sinitskikh <sinitskih@bk.ru>
2026-03-29 19:02:57 +0300

    added addition outputs

--- task_05_materials_vat.py
+++ task_05_materials_vat.py
@@ -12,10 +12,13 @@ total_price = price * quantity
 print("Итого: " + str(total_price))
 
 if total_price >= 1000 and total_price <= 5000:
+    print("Скидка 5%")
     final_price = total_price - (total_price*0.05)
 elif total_price >= 5000:
+    print("Скидка 10%")
     final_price = total_price - (total_price*0.1)
 else:
+    print("Скидки нет")
     final_price = total_price
 
 print(final_price)
