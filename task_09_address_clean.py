ceeb474becaa263a34c45f37d0670d35ecb746ea
Sinitskikh <sinitskih@bk.ru>
2026-04-03 21:20:07 +0300

    added basic logic

--- task_09_address_clean.py
+++ task_09_address_clean.py
@@ -5,4 +5,30 @@
 Цель: Работа со строками
 """
 
+addresses = [
+    "  г. Москва, ул. Ленина, д. 10 ",
+    "г.Казань,ул.Баумана,д.15",
+    " г. Санкт-Петербург, ул. Невский, д. 100 "
+]
+
+cleaned_addresses = []
+
+for addr in addresses:
+    res = addr.strip()
+    res = res.replace(" ,", ",").replace(",", ", ")
+    res = res.replace("г.", "г. ").replace("ул.", "ул. ").replace("д.", "д. ")
+    res = " ".join(res.split())
+    res = res.replace(",  ", ", ")
+
+    cleaned_addresses.append(res)
+
+print("=== СРАВНЕНИЕ ===")
+print("")
+
+for index, item in enumerate(cleaned_addresses):
+    print("#" + str(index + 1))
+    print(f"ДО: '{addresses[index]}'")
+    print(f"ПОСЛЕ: '{item}'")
+    print("")
+
 
