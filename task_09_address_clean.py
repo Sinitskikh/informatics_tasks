2003819ce26875cabdbc8c10aabaf9b82a9b8e4f
Sinitskikh <sinitskih@bk.ru>
2026-04-03 21:21:21 +0300

    added comments

--- task_09_address_clean.py
+++ task_09_address_clean.py
@@ -11,24 +11,29 @@ addresses = [
     " г. Санкт-Петербург, ул. Невский, д. 100 "
 ]
 
+# Создаем пустой массив для чистых адресов
 cleaned_addresses = []
 
 for addr in addresses:
     res = addr.strip()
+    # Исправляем запятые
     res = res.replace(" ,", ",").replace(",", ", ")
+    # Добавляем пробелы после сокращений
     res = res.replace("г.", "г. ").replace("ул.", "ул. ").replace("д.", "д. ")
+    #  Чистим двойные пробелы
     res = " ".join(res.split())
+    # Удаляем лишний пробел после замены запятой
     res = res.replace(",  ", ", ")
 
     cleaned_addresses.append(res)
 
+
 print("=== СРАВНЕНИЕ ===")
 print("")
 
+# Вывод результата
 for index, item in enumerate(cleaned_addresses):
-    print("#" + str(index + 1))
+    print("#" + str(index+1))
     print(f"ДО: '{addresses[index]}'")
     print(f"ПОСЛЕ: '{item}'")
     print("")
-
-
